b12ad073bcbdadbde417b40df798e86b19cdcac3
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-11 13:44:00 -0300

    feat(Variáveis e Strings): refatora @fernandesmelo

--- Variáveis e Strings/variaveisestrings.js
+++ Variáveis e Strings/variaveisestrings.js
@@ -22,12 +22,11 @@ const pokemon = "pikachu, 3, eletric, (thunderShock - QUICK attack - thuNder), S
 let partes = pokemon.split(",");
 
 //Transformando a primeira letra do picachu teste:
-partes[0] = partes[0].charAt(0).toUpperCase() + partes[0].slice(1);
+const pikachu = partes[0].charAt(0).toUpperCase() + partes[0].slice(1);
+const nivel = partes[1];
 
 //Formatando a palavra eletric:
-partes[2] = partes[2].toUpperCase();
-
-
+const electric = partes[2].toUpperCase();
 
 
 //Joy 
@@ -40,29 +39,44 @@ let attacks = partes[3].split("-");
 
 //Joyce 
 //Thundershock
-attacks[0] = attacks[0].trim();
-attacks[0] = attacks[0].charAt(0).toUpperCase() + attacks[0].slice(1);
+let thundershock = attacks[0].trim();
+thundershock = thundershock.charAt(0).toUpperCase() + thundershock.slice(1);
 
 //Quick 
-attacks[1] = attacks[1].trim();
-attacks[1] = attacks[1].charAt(0).toUpperCase() + attacks[1].slice(1);
+let quickAttack = attacks[1].trim();
+quickAttack = quickAttack.charAt(0).toUpperCase() + quickAttack.slice(1);
 
 //Thunder
-attacks[2] = attacks[2].trim();
-attacks[2] = attacks[2].charAt(0).toUpperCase() + attacks[2].slice(1);
+let thunder = attacks[2].trim();
+thunder = thunder.charAt(0).toUpperCase() + thunder.slice(1);
 
-partes[4] = partes[4].toLocaleLowerCase();
+const static = partes[4].toLocaleLowerCase();
 
 // Laércio 
-partes[5] = partes[5].replace("(", "").replace(")", "");
-partes[5] = partes[5].toLowerCase();
+let veridian = partes[5].replace("(", "").replace(")", "");
+veridian = veridian.toLowerCase();
 
-partes[6] = partes[6].toLowerCase();
+const cerulean = partes[6].toLowerCase();
 
-partes[7] = partes[7].replace("(", "").replace(")", "");
-partes[7] = partes[7].toLowerCase();
+let pallet = partes[7].replace("(", "").replace(")", "");
+pallet = pallet.toLowerCase();
 
-console.log(partes[7]);
+let resultado = 
+
+console.log(pallet);
+
+// Name: Pikachu
+//     Lv: 3 - Type: ELETRIC
+//     Ability: static
+//     Attacks:
+//         - Thundershock,
+//         - Quick attack
+//         - Thunder
+
+//     Places:
+//         - veridian
+//         - pallet
+//         - cerulean
 
 
 
