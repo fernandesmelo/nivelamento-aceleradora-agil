f9a4a7079cb6233019769e70ab70dc0be7873c4f
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-11 10:31:23 -0300

    Adiciona comentários

--- Variáveis e Strings/variaveisestrings.js
+++ Variáveis e Strings/variaveisestrings.js
@@ -18,10 +18,13 @@
 //Laercio
 const pokemon = "pikachu, 3, eletric, (thunderShock - QUICK attack - thuNder), STATIC, (VERIDIAN, CERULEAN, PALLET)"
 
+//Divide a string, utilizando a virgula como parametro de separação:
 let partes = pokemon.split(",");
 
+//Transformando a primeira letra do picachu:
 partes[0] = partes[0].charAt(0).toUpperCase() + partes[0].slice(1);
 
+//Formatando a palavra eletric:
 partes[2] = partes[2].toUpperCase();
 
 
