7638cddcb299c9c3947600f4fff71aba62fb2003
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-12 14:25:26 -0300

    feat(Condicionais): adiciona resolucao do erxercicio 2 e 4 - @fernandesmelo

---
+++ Condicionais (IF ELSE)/Lista 02/exercicio03.js
@@ -0,0 +1,14 @@
+// Faça um algoritmo que recebe 4 números inteiros, calcule e mostre a soma desses números.
+
+function soma (num1, num2, num3, num4) {
+    return num1 + num2 + num3 + num4
+}
+
+let num1 = Math.floor(Math.random() * 100);
+let num2 = Math.floor(Math.random() * 100);
+let num3 = Math.floor(Math.random() * 100);
+let num4 = Math.floor(Math.random() * 100);
+
+console.log(`Números Gerados: ${num1}, ${num2}, ${num3} e ${num4}`);
+
+console.log("Soma:", soma(num1, num2, num3, num4)); 
