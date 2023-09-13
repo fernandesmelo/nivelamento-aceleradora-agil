e4ad293c29b5300746ce9f7c291630a7a76ab032
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-13 15:43:35 -0300

    feat(Condicionais): Solucina exercícios 9 e 10 da Lista 2 - @joyce-caroline

--- 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio09.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio09.js
@@ -1,2 +1,18 @@
 // 9) Faça um programa que receba o salário base de um funcionário, calcule e mostre o salário a receber sabendo- se que o 
 // funcionário tem gratificação de 5% sobre o salário base e paga imposto de 7% sobre o salário com a gratificação.
+
+let salario = parseFloat((Math.random() * 5000).toFixed(2));
+
+console.log("Salário incial: R$", salario);
+
+const salarioComGratificacao = (salario) => {
+    return (salario + (salario* 0.05));
+}
+
+const salarioMenosImposto = (salarioComGratificacao) => {
+    return salarioComGratificacao - (salarioComGratificacao * 0.07);
+}
+
+console.log(`O salário com gratificação de 5% é R$${salarioComGratificacao(salario).toFixed(2)}`);
+console.log(`O salário total depois do imposto de 7% pago é R$${salarioMenosImposto(salarioComGratificacao(salario)).toFixed(2)}`);
+
--- 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio10.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio10.js
@@ -1,3 +1,39 @@
 // 10) Faça um programa que leia o nome e a data de nascimento de uma pessoa (dia, mês e ano), e após informe, junto ao nome 
 // da pessoa, quantos dias esta pessoa já viveu. Para simplificar o cálculo considere que todos os meses possuem 30 dias, e 
 // todos os anos 365 dias.
+
+let nome = "Laércio"
+let diaNasc = 7;
+let mesNasc = 9;
+let anoNasc = 1998;
+
+function diasDeVida(diaNasc, mesNasc, anoNasc){
+    const diasNoAno = 365;
+    const diasNoMes = 30;
+
+    const dataAtual = new Date();
+    const anoAtual = dataAtual.getFullYear();
+    const mesAtual = dataAtual.getMonth() + 1;
+    const diasAtual = dataAtual.getDate();
+
+
+    const dias = diasAtual - diaNasc;
+    const mes = mesAtual - mesNasc;
+    const anos = anoAtual - anoNasc;
+
+    const diasDeVida = (anos * diasNoAno) + (mes * diasNoMes) + dias;
+
+    return diasDeVida;
+}
+
+console.log(`${nome} tem ${diasDeVida(diaNasc, mesNasc, anoNasc)} dias de vida`);
+
+
+
+
+
+
+
+
+
+
