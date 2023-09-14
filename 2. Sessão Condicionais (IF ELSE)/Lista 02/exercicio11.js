2f57f4333013808701764d93b3ff55ee3406cc5c
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-14 11:46:44 -0300

    feat(Condicionais): soluciona exercicios 11 e 12 da lista 02 - @fernandesmelo

--- 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio11.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio11.js
@@ -1,3 +1,20 @@
 // 11)Faça um programa que leia duas informações do usuário, primeiro deve ser lido o SOBRENOME e depois o NOME de uma 
 // pessoa, e após o programa deve exibir a seguinte mensagem: "Sr(a). <NOME> <SOBRENOME>, é uma honra contar com sua 
 // presença!".
+
+const readline = require('readline');
+
+const rl = readline.createInterface ({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Digite o sobrenome: ', (sobrenome) => { 
+  rl.question('Digite o nome: ', (nome) => { 
+    // Fechar a interface de leitura
+    rl.close();
+
+    // Exibir a mensagem formatada
+    console.log(`Sr(a). ${nome} ${sobrenome}, é uma honra contar com sua presença!`);
+  });
+});
--- 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio12.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio12.js
@@ -1,2 +1,13 @@
 // 12) Faça um programa que receba o valor dos catetos de um triângulo, calcule e mostre o valor da hipotenusa. 2 
 // (h2=c12+c22).
+
+let cateto1 = Math.floor(Math.random() * (10 - 1) + 1);
+let cateto2 = Math.floor(Math.random() * (10 - 1) + 1);
+
+function calcularHipotenusa (cateto1, cateto2) {
+    const hipotenusa = Math.sqrt(cateto1 ** 2 + cateto2 ** 2);
+    return hipotenusa;
+}
+console.log("Cateto 1:", cateto1);
+console.log("Cateto 2:", cateto2);
+console.log(`A hipotenusa do triângulo é: ${calcularHipotenusa(cateto1, cateto2).toFixed(2)}`);
