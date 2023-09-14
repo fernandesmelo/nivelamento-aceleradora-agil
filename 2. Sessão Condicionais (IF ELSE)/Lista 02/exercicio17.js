a3dc3e64578214512c3eb4eae6da3514a04544c6
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-14 13:50:51 -0300

    feat(Condicionais): Soluciona exercícios 17 e 18 da Lista 02 - @joyce-caroline

--- 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio17.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio17.js
@@ -1,8 +1,30 @@
 // 17) Sabe-se que:
 // 1 pé = 12 polegadas;
-// 1 jarda = 3 pés;
+// 1 jarda = 3 pés = 36 polegadas;
 // 1 milha = 1760 jardas.
 // Faça um programa que receba uma medida em pés, faça as conversões a seguir e mostre os resultados.
 // a) Polegadas;
 // b) Jardas;
 // c) Milhas.
+
+const converteEmPolegadas = value => {
+    return value * 12;
+}
+
+const converteEmJardas = value => {
+    return value * 0.333333;
+}
+
+const converteEmMilhas = value => {
+    return value * 0.000189394;
+}
+
+let pe = parseInt(Math.random() * (100 - 1) + 1);
+
+console.log(`O valor em pés é ${pe}`, "\n");
+
+console.log(`${pe} pés = ${converteEmPolegadas(pe)} polegadas`);
+console.log(`${pe} pés = ${converteEmJardas(pe).toFixed(2)} jardas`);
+console.log(`${pe} pés = ${converteEmMilhas(pe).toFixed(2)} milhas`);
+
+
--- 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio18.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio18.js
@@ -1,3 +1,19 @@
 // 18) Faça um programa que receba o ano de nascimento de uma pessoa e o ano atual, calcule e mostre:
 // a) a idade desta pessoa;
 // b) quantos anos ela terá em 2050.
+
+let anoNascimento = 2011;
+
+const calculaIdadeHoje = anoNascimento => {
+    const anoAtual = new Date().getFullYear();
+    return anoAtual - anoNascimento;
+}
+
+const calculaIdade2050 = anoNascimento => {
+    return 2050 - anoNascimento;
+}
+
+console.log(`
+Hoje você tem ${calculaIdadeHoje(anoNascimento)} anos de idade.
+Em 2050 você terá ${calculaIdade2050(anoNascimento)} anos de idade
+`)
