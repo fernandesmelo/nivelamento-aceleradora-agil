5c349c7c47de25c7634e3d76c9bcf155b4dce65c
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-14 11:57:17 -0300

    feat(Condicionais): Soluciona exercícios 13 e 14 da Lista 02 - @joyce-caroline

--- 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio13.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio13.js
@@ -1,1 +1,11 @@
-// 13) Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: Área = (base * altura)/2.
+// 13) Faça um programa que calcule e mostre a área de um triângulo. Sabe-se que: 
+// Área = (base * altura)/2.
+
+let base = 16;
+let altura = 10;
+
+const areaTriangulo = (base, altura) => {
+    return (base * altura) / 2;
+}
+
+console.log(`A área do trinagulo de base ${base} e altura ${altura} é ${areaTriangulo(base, altura)}`);
--- 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio14.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 02/exercicio14.js
@@ -1,1 +1,10 @@
-// 14) Faça um programa que calcule e mostre a área de um círculo. Sabe-se que: Área = π * R2.
+// 14) Faça um programa que calcule e mostre a área de um círculo. 
+//Sabe-se que: Área = π * R2.
+
+let raio = parseInt(Math.random() * 10);
+
+const areaCirculo = (raio) => {
+    return Math.PI * Math.pow(raio, 2);
+}
+
+console.log(`A área do círculo de raio ${raio} é ${areaCirculo(raio).toFixed(2)}`);
