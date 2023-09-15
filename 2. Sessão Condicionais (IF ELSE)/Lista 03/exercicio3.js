73441409f7c58dc4e307fb441b94d311d366e54d
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-15 11:16:36 -0300

    feat(Condicionais): soluciona exercicios 03 e 04 da lista 03 - @fernandesmelo

--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio3.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio3.js
@@ -1,1 +1,9 @@
-//Construa um algoritmo que determine (imprima) se um dado númeor N inteiro, recebidoatravés do teclado é PAR ou ÍMPAR.
+//Construa um algoritmo que determine (imprima) se um dado número N inteiro, recebidoa através do teclado é PAR ou ÍMPAR.
+
+let numero = Math.floor(Math.random() * 100);
+
+if (numero % 2 === 0) {
+    console.log(numero, "é um número par.")
+} else {
+    console.log(numero, "é um número ímpar.");
+}
--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio4.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio4.js
@@ -1,1 +1,11 @@
 //Escreva um algoritmo para determinar se um dado número N recebido através do teclado, é POSITIVO, NEGATIVO ou NULO.
+
+let numero = 0;
+
+if (numero > 0) {
+    console.log(numero, "é um número positivo.");
+} else if (numero < 0) {
+    console.log(numero, "é um número negativo.");
+} else {
+    console.log(numero, "é um número nulo.");
+}
