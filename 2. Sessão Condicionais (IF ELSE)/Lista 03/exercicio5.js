57583496984a4acb810310370cec6ca8db26be9c
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-15 11:33:55 -0300

    feat(Condicionais): Soluciona exercícios 5 e 6 da Lista 03 - @joyce-caroline

--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio5.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio5.js
@@ -1,3 +1,24 @@
 // Construir um algoritmo que leia dois números e efetue a adição. Caso o valor somado seja maior 
-// que 20, este deverá ser apresentado somando-se a ele masi 87; caso o valor seja menor ou igual 
+// que 20, este deverá ser apresentado somando-se a ele mais 87; caso o valor seja menor ou igual 
 // a 20, este valor deverá ser apresentado subtraindo-se 5.
+
+
+let valor1 = Math.floor(Math.random() * 100);
+let valor2 = Math.floor(Math.random() * 100);
+
+
+const soma = (value, value2) => {
+    return value + value2;
+}
+
+const resultado = soma(valor1, valor2);
+
+console.log(`Os valores gerados são ${valor1} e ${valor2} => ${valor1} + ${valor2} = ${resultado}`);
+
+
+if(resultado > 20){
+    console.log(`O novo valor é ${resultado + 87}`);
+} else {
+    console.log(`O novo valor é ${resultado - 5}`);
+    
+}
--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio6.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio6.js
@@ -1,2 +1,10 @@
 // Escreva um algoritmo que leia um número e imprima a raiz quadrada do número, caso ele seja positivo ou igual a zero; 
 // e o quadrado do número, caso ele seja negativo.
+
+let numero = -10;
+
+if (numero >= 0) {
+    console.log(`${Math.sqrt(numero)} é a raiz quadrada de ${numero}.`);
+} else {
+    console.log(`${Math.pow(numero, 2)} é o quadrado do número ${numero}.`);
+} 
