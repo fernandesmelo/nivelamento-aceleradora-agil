88920a18e8b92009eb05d6cfb59192eeac775173
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-15 11:55:01 -0300

    feat(Condicionais): Soluciona exercícios 9 e 10 da Lista 03 - @joyce-caroline

--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio10.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio10.js
@@ -1,3 +1,22 @@
 // A prefeitura de Contagem abriu uma linha de crédito para os funcionários estatutários. O valor máximo da prestação não poderá 
 // ultrapassar 30% do salário bruto. Fazer um algoritmo que permita entrar com o salário bruto e o valor da prestação, e informar 
 // se o empréstimo pode ou não ser concedido.
+
+
+const readline = require('readline');
+
+let rl = readline.createInterface(
+                process.stdin, process.stdout);
+
+rl.question('Digite o valor do salário bruto: ', (salario) => { 
+    rl.question('Digite o valor da prestção: ', (prestacao) => { 
+        // Fechar a interface de leitura
+        rl.close();
+
+        if (prestacao > (salario * 0.3)) {
+            console.log("Empréstimo não pode ser concedido. Valor da prestação é maior que 30% do salário bruto.");
+        } else  {
+            console.log("Empréstimo pode ser concedido!");
+        }
+    });
+});
--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio9.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio9.js
@@ -1,1 +1,19 @@
 // Construa um algoritmo que imprima qual o menor e qual o maior valor de dois números A e B, lidos através do teclado.
+
+const readline = require('readline');
+
+let rl = readline.createInterface(
+                process.stdin, process.stdout);
+
+rl.question('Digite um número: ', (numA) => { 
+    rl.question('Digite mais um número: ', (numB) => { 
+        // Fechar a interface de leitura
+        rl.close();
+
+        if (numA > numB) {
+            console.log(numA, "é maior que", numB);
+        } else if (numA < numB) {
+            console.log(numA, "é menor que", numB);
+        }
+    });
+});
