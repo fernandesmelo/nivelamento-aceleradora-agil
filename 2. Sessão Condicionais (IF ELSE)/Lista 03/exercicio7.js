4e2bacdf5c41fe973ca0fece108c00e8801a04bf
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-15 11:46:43 -0300

    feat(Condicionais): soluciona exercicios 07 e 08 da lista 03 - @fernandesmelo

--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio7.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio7.js
@@ -1,1 +1,9 @@
 //Escreva um algoritmo que receba um número e imprima uma das mensagens: "é múltiplo de 3" ou "não é múltiplo de 3".
+
+let numero = Math.floor(Math.random() * 100);
+
+if (numero % 3 === 0) {
+    console.log(numero, "é múltiplo de 3.");
+} else {
+    console.log(numero, "não é múltiplo de 3.");
+}
--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio8.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio8.js
@@ -1,2 +1,20 @@
 // Escreva um algoritmo para determinar se um número A é divisível por um outro número B.
 // Esses valores devem ser fornecidos pelo usuário.
+
+const readline = require('readline');
+
+let rl = readline.createInterface(
+                process.stdin, process.stdout);
+
+rl.question('Digite um número: ', (numA) => { 
+    rl.question('Digite mais um número: ', (numB) => { 
+        // Fechar a interface de leitura
+        rl.close();
+
+        if (numA % numB === 0) {
+            console.log(numA, "é divisível por", numB);
+        } else {
+            console.log(numA, "não é divisível por", numB);
+        }
+    });
+});
