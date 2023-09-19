c93af8495d3ce0e00f2c1e346b6b7c628f3256ef
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-19 11:53:10 -0300

    feat(Condicionais): soluciona exercicios 11 e 12 da lista 03 - @fernandesmelo

--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio11.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio11.js
@@ -1,1 +1,36 @@
 // Construa um algoritmo que, dado quatro valores, A, B, C e D, o algoritmo diga qual é o maior e o menor valor.
+
+let a = Math.floor(Math.random() * 100);
+let b = Math.floor(Math.random() * 100);
+let c= Math.floor(Math.random() * 100);
+let d = Math.floor(Math.random() * 100);
+
+let maior = a;
+let menor = a;
+
+if (b > maior) {
+    maior = b;
+} else if (b < menor) {
+    menor = b;
+}
+
+if (c > maior) {
+    maior = c;
+} else if (c < menor) {
+    menor = c;
+}
+
+if (d > maior) {
+    maior = d;
+} else if (d < menor) {
+    menor = d;
+}
+
+console.log("Números Gerados:");
+console.log("A:", a);
+console.log("B:", b);
+console.log("C:", c);
+console.log("D:", d);
+console.log("O maior valor é: " + maior);
+console.log("O menor valor é: " + menor);
+
--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio12.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio12.js
@@ -1,1 +1,38 @@
 // Dados três valores A, B e C, construa um algoritmo que mostre os valores de forma ascendente (do menor para o maior).
+
+let a = Math.floor(Math.random() * 100);
+let b = Math.floor(Math.random() * 100);
+let c = Math.floor(Math.random() * 100);
+
+let menor, meio, maior;
+
+if (a <= b && a <= c) {
+    menor = a;
+    if (b <= c) {
+        meio = b;
+        maior = c;
+    } else {
+        meio = c;
+        maior = b;
+    }
+} else if (b <= a && b <= c) {
+    menor = b;
+    if (a <= c) {
+        meio = a;
+        maior = c;
+    } else {
+        meio = c;
+        maior = a;
+    }
+} else {
+    menor = c;
+    if (a <= b) {
+        meio = a;
+        maior = b;
+    } else {
+        meio = b;
+        maior = a;
+    }
+}
+console.log("Números Gerados:", a, b, c);
+console.log("Valores em ordem ascendente:", menor, meio, maior);
