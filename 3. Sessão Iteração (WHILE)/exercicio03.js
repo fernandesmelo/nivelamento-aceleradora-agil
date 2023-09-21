727ead0589de4bed5996bb15ce8a400594981ec5
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-21 11:19:18 -0300

    feat(For/While): soluciona exercicios 03 e 04 - @fernandesmelo

---
+++ 3. Sessão Iteração (WHILE)/exercicio03.js
@@ -0,0 +1,22 @@
+// 3) Faça um programa que leia dois números M e N e mostre todos os números entre estes valores na tela 
+// (o usuário deve informar um valor para M menor que o valor de N).
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface ({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Informe um número: ", (m) => {
+    rl.question("Informe um número maior que o primeiro: ", (n) => {
+        rl.close();
+        
+        console.log("Os números entre", m, "e", n, "são:");
+        m++
+        for (let menor = m; menor < n; menor++) {
+            console.log(menor);
+        }
+    })
+})
---
+++ 3. Sessão Iteração (WHILE)/exercicio04.js
@@ -0,0 +1,22 @@
+// 4) Faça um programa que leia dois números M e N e mostre apenas os números ímpares do intervalo.
+
+const readline = require('readline');
+
+const rl = readline.createInterface ({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Informe um número: ", (m) => {
+    rl.question("Informe um número maior que o primeiro: ", (n) => {
+        rl.close();
+        
+        console.log("Os números ímpares entre", m, "e", n, "são:");
+        m++
+        for (let menor = m; menor < n; menor++) {
+            if (menor % 2 !== 0) {
+                console.log(menor);
+            }
+        }
+    })
+})
