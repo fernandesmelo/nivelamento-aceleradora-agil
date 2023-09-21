605fbcd5397f7ae545f3ecc195b17659aa02823b
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-21 09:56:48 -0300

    feat(Condicionais): Soluciona exercícios 13 e 14 da Lista 03 - @joyce-caroline

--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio13.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio13.js
@@ -1,1 +1,43 @@
 //Dados três valores A, B e C, construa um algoritmo que mostre os valores de forma descendente.
+
+let a = Math.floor(Math.random() * 100);
+let b = Math.floor(Math.random() * 100);
+let c = Math.floor(Math.random() * 100);
+
+let maior;
+let meio;
+let menor;
+
+if (a > b && a > c) {
+    maior = a;
+    if (b > c) {
+        meio = b;
+        menor = c;
+    }else{
+        meio = c;
+        menor = b;
+    }
+
+} else if (b > a && b > c) {
+    maior = b;
+    if (a > c) {
+        meio = a;
+        menor = c;
+    }else{
+        meio = c;
+        menor = a;
+    }
+}else{
+    maior = c;
+    if (b > a) {
+        meio = b;
+        menor = a;
+    }else{
+        meio = a;
+        menor = b;
+    }
+}
+
+console.log(`
+Os números gerados são: ${a}, ${b} e ${c}.
+Os número sem ordem descendente: ${maior}, ${meio} e ${menor}. `)
--- 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio14.js
+++ 2. Sessão Condicionais (IF ELSE)/Lista 03/exercicio14.js
@@ -1,2 +1,14 @@
 // Escreva um algoritmo que leia um número e informe se ele é divisível por 10, por 5 ou por 2 ou se não é divisível por 
 // nenhum deles.
+
+let num = Math.floor(Math.random() * 100);;
+
+if(num % 10 === 0){
+    console.log(`O número ${num} é divísivel por 10.`);
+} else if(num % 5 === 0){
+    console.log(`O número ${num} é divísivel por 5.`);
+}else if(num % 2 === 0){
+    console.log(`O número ${num} é divísivel por 2.`);
+}else {
+    console.log(`O número ${num} não é divísivel por 10, 5 ou 2.`);
+}
