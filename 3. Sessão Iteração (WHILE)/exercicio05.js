6406681a945fd83814d5b9dc9930428fb825fe11
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-21 11:27:51 -0300

    feat(FOR/WHILE): Soluciona exercícios 5 e 6 - @joyce-caroline

---
+++ 3. Sessão Iteração (WHILE)/exercicio05.js
@@ -0,0 +1,27 @@
+//5) Faça um programa que leia dois números M e N. mostre todos os números pares de intervalo e ao final diga a quantidade 
+// de números pares apresentados na tela.
+
+let contador = 0;
+const readline = require('readline');
+
+const rl = readline.createInterface ({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Informe um número: ", (m) => {
+    rl.question("Informe um número maior que o primeiro: ", (n) => {
+        rl.close();
+        
+        console.log("Os números pares entre", m, "e", n, "são:");
+        m++
+        for (let menor = m; menor < n; menor++) {
+            if (menor % 2 === 0) {
+                console.log(menor);
+                contador++;
+            }
+        }
+        console.log(`Foram impressos ${contador} números pares`);
+    })
+})
+
---
+++ 3. Sessão Iteração (WHILE)/exercicio06.js
@@ -0,0 +1,24 @@
+//6) Faça um programa que leia dois números M e N e mostre todos os números divisíveis por 3 do intervalo.
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface ({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Informe um número: ", (m) => {
+    rl.question("Informe um número maior que o primeiro: ", (n) => {
+        rl.close();
+        
+        console.log("Os números pares entre", m, "e", n, "são:");
+        m++
+        for (let menor = m; menor < n; menor++) {
+            if (menor % 3 === 0) {
+                console.log(menor);
+            }
+        }
+    })
+})
+
