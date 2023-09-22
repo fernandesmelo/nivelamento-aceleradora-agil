c2357f9c88769e69f801ab45e7ac556158ececb7
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-22 11:17:54 -0300

    feat(For/While): soluciona exercicios 11 e 12 - @fernandesmelo

--- 3. Sessão Iteração (FOR WHILE)/exercicio11.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio11.js
@@ -1,2 +1,41 @@
 // 11) Faça um programa no qual o usuários insira valores numéricos positivos até digitar -1 e ao final diga quantos 
 // números foram digitados.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function contarNumeros() {
+  let numeros = [];
+  let numero;
+
+  function getInput() {
+    rl.question('Digite um número positivo (ou -1 para sair): ', (input) => {
+      numero = parseInt(input);
+
+      if (!isNaN(numero)) {
+        if (numero !== -1 && numero >= 0) {
+          numeros.push(numero);
+          getInput();
+        } else if (numero !== -1) {
+          console.log('Por favor, digite um número positivo.');
+          getInput();
+        } else {
+          rl.close();
+          const totalNumeros = numeros.length;
+          console.log(`Total de números digitados: ${totalNumeros}`);
+        }
+      } else {
+        console.log('Por favor, digite um número válido.');
+        getInput();
+      }
+    });
+  }
+
+  getInput();
+}
+
+contarNumeros();
--- 3. Sessão Iteração (FOR WHILE)/exercicio12.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio12.js
@@ -1,2 +1,40 @@
 // 12) Faça um programa no qual o usuário insira valores numéricos positivos até digitar -1 e ao final o diga o somatório 
 // dos valores digitados.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function somaNumeros() {
+  let soma = 0;
+  let numero;
+
+  function getInput() {
+    rl.question('Digite um número positivo (ou -1 para sair): ', (input) => {
+      numero = parseInt(input);
+
+      if (!isNaN(numero)) {
+        if (numero !== -1 && numero >= 0) {
+          soma = soma + numero;
+          getInput();
+        } else if (numero !== -1) {
+          console.log('Por favor, digite um número positivo.');
+          getInput();
+        } else {
+          rl.close();
+          console.log(`O somatório dos números digitados são: ${soma}`);
+        }
+      } else {
+        console.log('Por favor, digite um número válido.');
+        getInput();
+      }
+    });
+  }
+
+  getInput();
+}
+
+somaNumeros();
