ee781c1f5e6567fb6bdc233f4e8d054797e45a27
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-22 14:00:12 -0300

    feat(For/While): soluciona exercicios 15 e 16 - @fernandesmelo

--- 3. Sessão Iteração (FOR WHILE)/exercicio15.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio15.js
@@ -1,2 +1,61 @@
 // 15) Faça um algoritmo que leia uma quantidade não determinada de números positivos. Calcule a quantidade de números 
 // pares e impares, a média de valores pares e a média geral dos números lidos. O número que encerrará a leitura será zero.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function calculaMedia() {
+  let qtdPar = 0;
+  let qtdImpar = 0;
+  let qtdGlobal = 0;
+  let numero;
+  let somaPar = 0;
+  let mediaPar = 0;
+  let somaGlobal = 0;
+  let mediaGlobal = 0;
+
+  function getInput() {
+    rl.question('Digite um número positivo (ou 0 para sair): ', (input) => {
+      numero = parseInt(input);
+
+      if (!isNaN(numero)) {
+        if (numero !== 0 && numero > 0) {
+          
+          if(numero % 2 === 0) {
+            qtdPar++;
+            somaPar = somaPar + numero;
+            mediaPar = somaPar / qtdPar;
+          } else {
+            qtdImpar++;
+          }
+          
+          qtdGlobal = qtdPar + qtdImpar;
+          somaGlobal = somaGlobal + numero;
+          mediaGlobal = somaGlobal / qtdGlobal;
+          
+          getInput();
+        } else if (numero !== 0) {
+          console.log('Por favor, digite um número positivo.');
+          getInput();
+        } else {
+          rl.close();
+          console.log(`Números Pares: ${qtdPar}`);
+          console.log(`Números Ímpares: ${qtdImpar}`);
+          console.log(`Média dos Números Pares: ${mediaGlobal}`);
+          console.log(`Média dos Números Globais: ${mediaGlobal}`);
+        }
+      } else {
+        console.log('Por favor, digite um número válido.');
+        getInput();
+      }
+    });
+  }
+
+  getInput();
+}
+
+calculaMedia();
--- 3. Sessão Iteração (FOR WHILE)/exercicio16.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio16.js
@@ -1,3 +1,21 @@
 // 16) Faça um programa no qual o usuário informe o número de médias a serem inseridas e após leia cada uma das médias 
 // juntamente com o nome do aluno. Ao final o programa deverá informar a maior média juntamente com o nome do aluno que 
 // a obteve.
+
+const numMedias = parseInt(prompt("Informe o número de médias a serem inseridas:"));
+
+let maiorMedia = -1;
+let nomeAlunoMaiorMedia = "";
+
+for (let i = 1; i <= numMedias; i++) {
+  const nomeAluno = prompt(`Informe o nome do aluno ${i}:`);
+  const media = parseFloat(prompt(`Informe a média do aluno ${i}:`));
+
+  if (media > maiorMedia) {
+    maiorMedia = media;
+    nomeAlunoMaiorMedia = nomeAluno;
+  }
+}
+
+console.log(`A maior média é ${maiorMedia}, obtida por ${nomeAlunoMaiorMedia}.`);
+
