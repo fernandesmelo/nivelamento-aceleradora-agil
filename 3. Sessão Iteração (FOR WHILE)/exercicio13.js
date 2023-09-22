c7f6953ae3f34b02557593fb587765e0c642ae69
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-22 11:35:11 -0300

    feat(FOR/ While): Soluciona os exercícios 13 e 14. - @joyce-caroline

--- 3. Sessão Iteração (FOR WHILE)/exercicio13.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio13.js
@@ -1,2 +1,45 @@
 // 13) Faça um programa no qual o usuário insira valores numéricos positivos até digitar -1 e ao final informe quantos 
 // números pares e quantos números impares foram digitados.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function contaParImpar() {
+  let contPar = 0;
+  let contImpar = 0;
+  let numero;
+
+  function getInput() {
+    rl.question('Digite um número positivo (ou -1 para sair): ', (input) => {
+      numero = parseInt(input);
+
+      if (!isNaN(numero)) {
+        if (numero !== -1 && numero >= 0) {
+          if(numero % 2 ===0){
+            contPar++;
+          }else{
+            contImpar++;
+          }
+          getInput();
+        } else if (numero !== -1) {
+          console.log('Por favor, digite um número positivo.');
+          getInput();
+        } else {
+          rl.close();
+          console.log(`O total de números pares digitados é ${contPar} e o total de números ímpares digitados é ${contImpar}`);
+        }
+      } else {
+        console.log('Por favor, digite um número válido.');
+        getInput();
+      }
+    });
+  }
+
+  getInput();
+}
+
+contaParImpar();
--- 3. Sessão Iteração (FOR WHILE)/exercicio14.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio14.js
@@ -1,2 +1,47 @@
 // 14) Faça um programa no qual o usuário insira valores numéricos positivos até digitar -1 e ao final informe entre os 
 // números digitados quantos são: (a) múltiplos de 2 e 5, simultaneamente; (b) múltiplos de 2 e 3.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function verificaMultiplos() {
+  let multiplos2e5 = [];
+  let multiplos2e3 = [];
+  let numero;
+
+  function getInput() {
+    rl.question('Digite um número positivo (ou -1 para sair): ', (input) => {
+      numero = parseInt(input);
+
+      if (!isNaN(numero)) {
+        if (numero !== -1 && numero >= 0) {
+
+          if(numero % 2 === 0 && numero % 5 === 0){
+            multiplos2e5.push(numero);
+          }else if (numero % 2 === 0 && numero % 3 === 0){
+            multiplos2e3.push(numero);
+          }
+
+          getInput();
+        } else if (numero !== -1) {
+          console.log('Por favor, digite um número positivo.');
+          getInput();
+        } else {
+          rl.close();
+          console.log(`Os números ${multiplos2e3} são múltiplos de 2 e 3, e os números ${multiplos2e5} são múltiplos de 2 e 5.`);
+        }
+      } else {
+        console.log('Por favor, digite um número válido.');
+        getInput();
+      }
+    });
+  }
+
+  getInput();
+}
+
+verificaMultiplos();
