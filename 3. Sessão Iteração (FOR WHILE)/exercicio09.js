ea0baba177e0eb6329c6cf8c0bfb0a415a30a9b8
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-22 10:22:00 -0300

    feat(FOR/ While): Soluciona os exercícios 9 e 10. - @joyce-caroline

--- 3. Sessão Iteração (FOR WHILE)/exercicio09.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio09.js
@@ -1,1 +1,22 @@
 // 9) Faça um programa que leia N números e ao final mostre os 3 maiores números digitados.
+
+const numeros = (quantity) => {
+    let listaNumeros = [];
+
+    //Leitura dos números
+    for(let i = 0; i < quantity; i++) {
+        listaNumeros.push(Math.floor(Math.random() * 100));
+    }
+
+    const listaOrdenada = listaNumeros.sort();
+
+
+    return `
+    Os três maiores números do array [${listaOrdenada}] são:
+    ${listaOrdenada[quantity - 1]}, ${listaOrdenada[quantity - 2]} e ${listaOrdenada[quantity - 3]}.
+    `;
+
+
+}
+
+console.log(numeros(10));
--- 3. Sessão Iteração (FOR WHILE)/exercicio10.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio10.js
@@ -1,3 +1,24 @@
 // 10) Faça um programa que leia um número não determinado de pares de valores [M,N], todos inteiros e positivos, um par 
 // de cada vez, e que calcule e mostre a soma de todos os números inteiros de M até N (inclusive). A digitação de pares 
 // terminará quando M for maior ou igual a N.
+
+
+const somaIntervalo = (M, N) => {
+    if(M >= 0 && N >= 0 && Number.isInteger(M) && Number.isInteger(N)) {
+        let soma = 0;
+
+        for(let numero = M; numero <=N; numero++) {
+            soma += numero;
+        }
+
+        return `A soma do intervalo entre ${M} e ${N} é ${soma}`;
+
+    }else{
+            return 'Valores inválidos';
+    }
+
+    
+}
+
+
+console.log(somaIntervalo(2, 9));
