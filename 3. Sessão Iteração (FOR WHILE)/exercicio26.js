e5ffa9bcab14410dbae803ce8dd54762d232ffc1
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-26 14:44:08 -0300

    feat(FOR/WHILE): Soluciona exercícios 25 e 26 - @joyce-caroline

--- 3. Sessão Iteração (FOR WHILE)/exercicio26.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio26.js
@@ -1,6 +1,69 @@
 // 26) A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre o salário e número de 
 // filhos. A prefeitura deseja saber:
-// a. Média do salário da população; b. média do número de filhos;
+// a. Média do salário da população; 
+// b. média do número de filhos;
 // b. Maior salário;
 // c. Percentual de pessoas com salário até R$100,00.
 // O final da leitura de dados se dará com a entrada de um salário negativo.
+
+const mediaSalario = (array) =>{
+    let somaSalarios = 0;
+    for(let i = 0; i < array.length; i++){
+        somaSalarios += array[i].salario;
+    }
+    return (somaSalarios / array.length);
+}
+
+const mediaNumeroFilhos = (array) =>{
+    let somaFilhos = 0;
+    for(let i = 0; i < array.length; i++){
+        somaFilhos += array[i].numeroFilhos;
+    }
+    return (somaFilhos / array.length);
+}
+
+const maiorSalario = (array) => {
+    let maior = array[0].salario;
+    for(let i = 1; i < array.length; i++) {
+        if(array[i].salario > maior) {
+            maior = array[i].salario;
+        }   
+    }
+    return maior;
+}
+
+const percentualPessoas = (array) => {
+    let somaPessoas = 0;
+    for(let j = 0; j < array.length; j++){
+        if(array[j].salario === 100){
+            somaPessoas++;
+        }
+    }
+    return (somaPessoas/array.length) * 100;
+}
+
+let dadosPopulacao = [
+    {
+        salario: 100.00,
+        numeroFilhos: 2
+    },
+    {
+        salario: 1000.00,
+        numeroFilhos: 7
+    },
+    {
+        salario: 1800.00,
+        numeroFilhos: 10
+    },
+    {
+        salario: 1020.00,
+        numeroFilhos: 5
+    }
+]
+
+console.log(`A média salarial da população é  R$${mediaSalario(dadosPopulacao).toFixed(2)}`); 
+console.log(`A média de número de filhos é de ${mediaNumeroFilhos(dadosPopulacao)} filhos`); 
+console.log(`O maior salário é R$${maiorSalario(dadosPopulacao)}`); 
+console.log(`O percentual de pessoas que recebem R100,00 de salário é de ${percentualPessoas(dadosPopulacao)}%`); 
+
+
--- 3. Sessão Iteração (FOR WHILE)/exercicio27.js
+++ 3. Sessão Iteração (FOR WHILE)/exercicio27.js
@@ -1,2 +1,16 @@
 // 27) Chico tem 1,50 metro e cresce 2 centimetros por ano, enquanto Zé tem 1,10 metro e cresce 3 centimetros por ano. 
 // Construa um algoritmo que calcule e imprima quantos anos serão necessários para que Zé seja maior que Chico.
+
+let alturaChico = 1.50;
+let alturaZe = 1.10;
+let idade = 0;
+
+while(alturaChico > alturaZe){
+    alturaChico += 0.02;
+    alturaZe += 0.03;
+    idade++;
+}
+
+console.log(`Serão necessários ${idade} anos para Zé ter mais altura que Chico`);
+
+
