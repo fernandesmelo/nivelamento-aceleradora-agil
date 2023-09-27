8f5a64bbbf5d8b865cf95391ea498b32f027c926
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-27 11:24:47 -0300

    feat(Arrays): soluciona exercicio 02 - @fernandesmelo

--- 4. Exercícios Arrays/exercicio02.js
+++ 4. Exercícios Arrays/exercicio02.js
@@ -3,4 +3,24 @@
 
 // Função para criar o segundo vetor com posições pares e ímpares modificadas
 
-  
+function criarVetorModificado() {
+    const vetorOriginal = new Array(30);
+    const vetorModificado = [];
+
+    for (let i = 0; i < 30; i++) {
+        const valor = parseInt(prompt(`Digite o valor da posição ${i + 1}:`))
+        vetorOriginal[i] = valor;
+    }
+
+    for (let i = 0; i < 30; i++) {
+        if (i % 2 === 0) {
+            vetorModificado.push(vetorOriginal[i] * 2);
+        } else {
+            vetorModificado.push(vetorOriginal[i] * 3)
+        }
+    }
+
+    console.log("vetor Modificado:", vetorModificado);
+}
+
+criarVetorModificado();
