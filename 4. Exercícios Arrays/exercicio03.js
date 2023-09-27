be4ccb74b98d123e9c1072dcaa5c8ee249057678
joyce-caroline <caroline.joyce5631@gmail.com>
2023-09-27 11:42:41 -0300

    feat(Arrays): Soluciona exercícios 3 e 4. - @joyce-caroline

--- 4. Exercícios Arrays/exercicio03.js
+++ 4. Exercícios Arrays/exercicio03.js
@@ -1,3 +1,27 @@
 // 3. Faça um programa que preencha um vetor com oito números inteiros, calcule e mostre dois vetores resultantes. O
 // primeiro deve conter os números positivos; o segundo deve conter os números negativos. Cada vetor resultante vai ter, 
 // no máximo, oito posições, que poderão não ser completamente utilizadas.
+
+let array = [-1, -3, 0, -2, -1, 4, 8, 10];
+
+const vetoresResultantes = (array) =>{
+    let vetorPositivos = [];
+    let vetorNegativos = [];
+
+    for (let i=0; i<array.length; i++){
+        if (array[i] >= 0) {
+            vetorPositivos.push(array[i]);
+        }else{
+            vetorNegativos.push(array[i]);
+        }
+    }
+
+    console.log(`
+    Vetor Positivo: ${vetorPositivos}
+    Vetor Negativo: ${vetorNegativos}
+    `)
+
+    return;
+}
+
+vetoresResultantes(array);
--- 4. Exercícios Arrays/exercicio04.js
+++ 4. Exercícios Arrays/exercicio04.js
@@ -1,3 +1,23 @@
 // 4. Desenvolva um algoritmo que permita a leitura de um vetor de 30 números inteiros, e gere um segundo vetor com os
 // mesmos dados, mas de maneira invertida, ou seja, o primeiro elemento ficará na última posição, o segundo na penúltima, 
 // e assim por diante.
+
+// const geraVetor = () =>{
+//     let array = new Array(30);
+//     for (let i = 0; i < array.length; i++){
+//         array.push(Math.floor(Math.random() * 50));
+//     }
+//     console.log(array);
+    
+//     return array;
+// }
+
+
+
+const vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+const vetorReverso = (lista) =>{
+    return lista.reverse();
+}
+
+console.log(vetorReverso(vetor));
