027b6e6728f9e326037fd8e3c90468537af5fd2a
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-27 10:01:15 -0300

    feat(Arrays): soluciona exercicio 01 - @fernandesmelo

--- 4. Exercícios Arrays/exercicio01.js
+++ 4. Exercícios Arrays/exercicio01.js
@@ -1,4 +1,16 @@
 // 1. Crie um algoritmo que leia um vetor de 10 posições depois apresente este vetor em ordem inversa na tela.
 
+function vetorEmOrdemInversa() {
+  const vetor = new Array(10);
 
-  
+  for (let i = 0; i < 10; i++) {
+    const valor = parseInt(prompt(`Digite o valor da posição ${i + 1}:`))
+    vetor[i] = valor;
+  }
+
+  const vetorInverso = vetor.reverse();
+
+  console.log("Vetor em ordem inversa:", vetorInverso);
+}
+
+vetorEmOrdemInversa();
--- 4. Exercícios Arrays/exercicio02.js
+++ 4. Exercícios Arrays/exercicio02.js
@@ -1,2 +1,6 @@
 // 2. Crie um algoritmo que leia um vetor de 30 números inteiros e gere um segundo vetor cujas posições pares são o
 // dobro do vetor original e as ímpares o triplo.
+
+// Função para criar o segundo vetor com posições pares e ímpares modificadas
+
+  
