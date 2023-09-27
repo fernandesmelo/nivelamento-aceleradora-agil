2a9c7ac6e882ccbe74df25a0c55b42edda113e36
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2023-09-27 13:05:14 -0300

    feat(Arrays): soluciona exercicio 05 - @fernandesmelo

--- 4. Exercícios Arrays/exercicio05.js
+++ 4. Exercícios Arrays/exercicio05.js
@@ -1,2 +1,33 @@
 // 5. Faça um programa que preencha dois vetores de dez elementos numéricos cada um e mostre o vetor resultante da
 // intercalação deles.
+
+function preencherVetores() {
+    const vetor1 = [];
+    const vetor2 = [];
+
+    for (let i = 0; i < 10; i++) {
+        const valor = parseInt(prompt(`Digite o valor do primeiro vetor na posição ${i + 1}:`))
+        vetor1.push(valor);
+    }
+
+    for (let i = 0; i < 10; i++) {
+        const valor = parseInt(prompt(`Digite o valor do segundo vetor na posição ${i + 1}:`))
+        vetor2.push(valor);
+    }
+
+    const vetorIntercalado = intercalarVetores(vetor1, vetor2);
+    console.log("Vetor Intercalado:", vetorIntercalado);
+}
+
+function intercalarVetores(vetor1, vetor2) {
+    const vetorIntercalado = [];
+    
+    for (let i = 0; i < 20; i++) {
+        vetorIntercalado.push(vetor1[i]);
+        vetorIntercalado.push(vetor2[i]);
+    }
+
+    return vetorIntercalado;
+}
+
+preencherVetores();
--- 4. Exercícios Arrays/exercicio06.js
+++ 4. Exercícios Arrays/exercicio06.js
@@ -1,2 +1,3 @@
 // 6. Elabore um algoritmo que leia uma série de 50 notas e uma média necessária para aprovação, calcule e mostre
 // quantas notas estão em um intervalo de valores que vai de 10% abaixo da média até 10% acima dela.
+
